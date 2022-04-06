074564a0e5c218cc4a9a8fe743504d03263084b2
Martin Reece <martin@Martins-Air.home>
2022-04-06 08:27:09 -0400

    I added another variable name counter and left some comments to why it's written in the JS File. Also I added an addEventListener to my variables add and decrease with comments as well

--- verb.js
+++ verb.js
@@ -7,4 +7,20 @@ let decrease = document.getElementById('decrement');
 // Create a variable, give it a name called "int" and grab the div element named "number" from the DOM by using the document.getElementById() method. I can use this to read or edit(modify) an HTML element
 let int = document.getElementById('number');
 
+// Create a variable, give it a name called "counter" and set it to 0.  This will be used as a default reference.
+let counter = 0;
 
+// This event handler (addEventListener) is to target my variable "add" and monitor events such as when someone clicks on let's say the plus sign (+), it will increase the value by 1.
+// Also I use the variable (int) and assigned it to the innerHTML property so it can modify the content of my HTML element.  This will also be equal to my default reference (counter) as a link.
+add.addEventListener('click', function(){
+    counter += 1;
+    int.innerHTML = counter;
+})
+
+
+// This event handler (addEventListener) is to target my variable "decrease" and monitor events such as when someone clicks on let's say the minus sign (-), it will decrease the value by 1.
+// Also I use the variable (int) and assigned it to the innerHTML property so it can modify the content of my HTML element.  This will also be equal to my default reference (counter) as a link.
+decrease.addEventListener('click', function(){
+    counter -= 1;
+    int.innerHTML = counter;
+})
