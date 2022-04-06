379e74dd6c12bf9c9aecabc7951444a4458b9f2d
Martin Reece <martin@Martins-Air.home>
2022-04-06 08:14:57 -0400

    I added some comments and created some variables to read or edit an HTML element by using the document.getElementById() method.

---
+++ verb.js
@@ -0,0 +1,10 @@
+// Create a variable, give it a name called "add" and grab the div element named "increment" from the DOM by using the document.getElementById() method. I can use this to read or edit(modify) an HTML element
+let add = document.getElementById('increment');
+
+// Create a variable, give it a name called "decrease" and grab the div element named "decrement" from the DOM by using the document.getElementById() method. I can use this to read or edit(modify) an HTML element
+let decrease = document.getElementById('decrement');
+
+// Create a variable, give it a name called "int" and grab the div element named "number" from the DOM by using the document.getElementById() method. I can use this to read or edit(modify) an HTML element
+let int = document.getElementById('number');
+
+
